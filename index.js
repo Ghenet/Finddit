691752841f37d0e43cbd6031acfa5f8bf83d4aab
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-07-21 10:54:21 -0700

    gets the sort item by relavance or latest

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const searchInput = document.getElementById('search-input');
 searchForm.addEventListener('submit', e => {
     //Get search term
     const searchTerm = searchInput.value;
-    console.log(searchTerm);
+
+    //Get sort
+    const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+    console.log(sortBy);
     e.preventDefault();
 })
