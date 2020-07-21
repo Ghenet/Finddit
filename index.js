0f03da81658b4685473d84d79c1c63e7b1e7367f
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-07-21 11:48:48 -0700

    checks input and adds error msg for if searchbar is empty

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
+//Form eventListener
 searchForm.addEventListener('submit', e => {
     //Get search term
     const searchTerm = searchInput.value;
@@ -11,6 +12,28 @@ searchForm.addEventListener('submit', e => {
     //Get limit
     const searchLimit = document.getElementById('limit').value;
 
-    console.log(searchLimit);
+
+    //Check input 
+    if (searchTerm === '') {
+        //Show message
+        showMessage('Please add a search term', 'alert-danger');
+    }
     e.preventDefault();
-})
+});
+
+//Show message
+function showMessage(message, className) {
+    //Create div
+    const div = document.createElement('div');
+    //Add classes
+    div.className = `alert ${className}`;
+    //Add text
+    div.appendChild(document.createTextNode(message));
+    //Get parent
+    const searchContainer = document.getElementById('search-container');
+    //Get search
+    const search = document.getElementById('search');
+
+    //Insert message
+    searchContainer.insertBefore(div, search);
+}
